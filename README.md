bbcf48f6a5b973e82fc8538df529c8c15a33b7bf
davyengone <engonenzedavy@yahoo.com>
2016-10-23 15:54:52 +0200

    learning javascript the right way

---
+++ README.md
@@ -0,0 +1,13 @@
+## JavaScript Koans: Advanced concepts
+
+In this set of examples and tests we cover advanced JavaScript concepts such as closure, inheritance, variable hoisting, 'this' with context switch...
+
+Clone this repository from Github and run `npm install` or `npm i`
+
+Then you can run the test with `npm test`
+
+We use Mocha in this repository.
+
+This repository is also configured to run with WallabyJS.
+
+Feel free to send us PRs
---
+++ wallaby.js
@@ -0,0 +1,27 @@
+var babel = require('babel');
+
+module.exports = function (wallaby) {
+  return {
+    files: [
+      'examples/*.js',
+      'src/*.js'
+    ],
+
+    tests: [
+      'test/*spec.js'
+    ],
+
+    env: {
+      type: 'node'
+    },
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel({
+        babel: babel,
+        // https://babeljs.io/docs/usage/experimental/
+        stage: 0,
+        optional: ['es7.comprehensions', 'runtime']
+      })
+    }
+  };
+};
