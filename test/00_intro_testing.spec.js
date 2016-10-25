51b561b4eeb73aefc48035d1265669cc2f28139c
davyengone <engonenzedavy@yahoo.com>
2016-10-25 03:07:55 +0200

    fixed tests in intro

--- test/00_intro_testing.spec.js
+++ test/00_intro_testing.spec.js
@@ -1,27 +1,24 @@
-/**
-* Introduction to testing framework (Mocha)
-**/
-var expect = require('chai').expect;
+const expect = require('chai').expect;
 
 // documentation for the chai#expect http://chaijs.com/api/bdd/
 
-describe('Introductio to Mocha and WallabyJS', function() {
+xdescribe('Introductio to Mocha and WallabyJS', () => {
 
   // You can disable test by adding an 'x' in front of the runner
-  xit('Disable this test', function(){
-    var disabled = __;
+  it('Disable this test', () => {
+    const disabled = __;
     expect(disabled).to.be.false;
   });
 
   // We shall contemplate truth by testing reality, via spec expectations.
-  xit('true vs false', function() {
-    let truth = __;
-    let falsy = __;
+  it('true vs false', () => {
+    const truth = __;
+    const falsy = __;
     expect(truth).to.be.ok;
     expect(falsy).to.not.be.ok;
   });
 
-  xit('List all the falsy values in JavaScript', function() {
+  it('List all the falsy values in JavaScript', () => {
     let falsy = __;
     expect(falsy).to.not.be.ok;
     falsy = __;
@@ -35,19 +32,19 @@ describe('Introductio to Mocha and WallabyJS', function() {
   });
 
   // Expected value versus actual value
-  xit('should expect equality', function() {
-    let expectedValue = __;
-    let actualValue = 1 + 1;
+  it('should expect equality',() => {
+    const expectedValue = __;
+    const actualValue = 1 + 1;
 
     expect(actualValue === expectedValue).to.be.ok;
   });
 
   // Sometimes you need to be precise about what you "type."
-  xit('should assert equality with ===', function() {
-    let expectedValue = __;
-    let actualValue = (1 + 1).toString();
+  it('should assert equality with ===', () => {
+    const expectedValue = __;
+    const actualValue = (1 + 1).toString();
 
-  // toBe() will always use === to compare.
+  // toEqual() will always use === to compare.
     expect(actualValue).to.equal(expectedValue);
   });
 });
