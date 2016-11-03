93a597a0debefe391f9dea1233d215d91287e508
Boubacar S. BARRY <bouba@hackages.io>
2016-11-03 08:22:48 +0100

    replace arrow function by function in lodash.spec.js

--- test/lodash.spec.js
+++ test/lodash.spec.js
@@ -5,35 +5,35 @@ chai.should();
 
 const _ = require('../src/lodash');
 
-describe('Lodash library', ()=>{
+describe('Lodash library', function(){
 
-  describe('identity', ()=>{
+  describe('identity', function(){
 
-    it('Should return the same value that has been passed', ()=>{
+    it('Should return the same value that has been passed', function(){
       expect(_.identity(1)).equal(1);
       expect(_.identity({})).to.deep.equal({});
       expect(_.identity('hackages')).to.deep.equal('hackages');
     })
   });
 
-  describe('first: does not mutate the collection', ()=>{
+  describe('first: does not mutate the collection', function(){
 
     let items;
     beforeEach(function(){
       items = [1,9, 10, true, 'Hacklunch', {name: 'Hackages'}];
     });
 
-    it('Should throw an error if no argument is passed', ()=>{
+    it('Should throw an error if no argument is passed', function(){
           (function(){
             _.first()
           }).should.throw(Error);
     });
 
-    it('Should return the first element of the collection', ()=>{
+    it('Should return the first element of the collection', function(){
       expect(_.first(items)).equal(1);
     });
 
-    it('Should return the first n elements when a second argument is passed', ()=>{
+    it('Should return the first n elements when a second argument is passed', function(){
       expect(_.first(items, 0)).equal(1);
       expect(_.first(items, 1)).to.deep.equal([1]);
       expect(_.first(items, 2)).to.deep.equal([1, 9]);
@@ -41,24 +41,24 @@ describe('Lodash library', ()=>{
     })
   });
 
-  xdescribe('last',  ()=>{
+  xdescribe('last',  function(){
     let items;
 
     beforeEach(function(){
       items = [1,9, 10, true, 'Hacklunch', {name: 'Hackages'}];
     });
 
-    it('Should throw an error if no collection is passed', ()=>{
+    it('Should throw an error if no collection is passed', function(){
         (function(){
           _.last();
         }).should.throw(Error);
     });
 
-    it('Should return the last element of the collection', ()=>{
+    it('Should return the last element of the collection', function(){
         expect(_.last(items)).to.deep.equal({name: 'Hackages'});
     });
 
-    it('Should return the last n elements when a second argument is passed', ()=>{
+    it('Should return the last n elements when a second argument is passed', function(){
       expect(_.last(items, 0)).to.deep.equal({name: 'Hackages'});
       expect(_.last(items, 1)).to.deep.equal([{name: 'Hackages'}]);
       expect(_.last(items, 2)).to.deep.equal(['Hacklunch', {name: 'Hackages'}]);
@@ -66,22 +66,22 @@ describe('Lodash library', ()=>{
     });
   });
 
-  xdescribe('isArray',  ()=>{
+  xdescribe('isArray',  function(){
     const items = [10, false, 'Hacklunch'];
 
-    it('Should return true when passed an collection of elements', ()=>{
+    it('Should return true when passed an collection of elements', function(){
       expect(_.isArray(items)).equal(true);
       expect(_.isArray([])).equal(true);
     });
 
-    it('Should return false when passed anything else', ()=>{
+    it('Should return false when passed anything else', function(){
       expect(_.isArray("Bootcamp by Hackages")).to.equal(false);
       expect(_.isArray({})).to.equal(false);
       expect(_.isArray(12)).to.equal(false);
       expect(_.isArray(false)).to.equal(false);
     });
 
-    it('arguments is not an Array', ()=>{
+    it('arguments is not an Array', function(){
       _.isArray(items)
 
       function isArray(){
@@ -90,8 +90,8 @@ describe('Lodash library', ()=>{
     });
   });
 
-  xdescribe('isDefined',  ()=>{
-    it('Happy path', ()=>{
+  xdescribe('isDefined',  function(){
+    it('Happy path', function(){
       expect(_.isDefined([])).equal(true);
       expect(_.isDefined(12)).equal(true);
       expect(_.isDefined(0)).equal(true);
@@ -100,29 +100,29 @@ describe('Lodash library', ()=>{
       expect(_.isDefined([])).equal(true);
     });
 
-    it('Should return false when passed a string', ()=>{
+    it('Should return false when passed a string', function(){
       let items;
       expect(_.isDefined(items)).equal(false);
     });
   });
 
-  xdescribe('isFunction',  ()=>{
-    it('Should return true when passed an anonymous function', ()=>{
-      _.isFunction(()=>{}).equal(true);
-      _.isFunction(()=>{}).equal(true);
+  xdescribe('isFunction',  function(){
+    it('Should return true when passed an anonymous function', function(){
+      _.isFunction(function(){}).equal(true);
+      _.isFunction(function(){}).equal(true);
     });
 
-    it('Should return true when passed a named function', ()=>{
+    it('Should return true when passed a named function', function(){
       const sum = (x, y)=> x + y;
       expect(_.isFunction(sum)).equal(true);
     });
 
-    it('Should return true when passed a class', ()=>{
+    it('Should return true when passed a class', function(){
       class Book{}
       expect(_.isFunction(Book)).equal(true);
     });
 
-    it('Should return false otherwise', ()=>{
+    it('Should return false otherwise', function(){
       expect(_.isFunction({})).equal(false);
       expect(_.isFunction(12)).equal(false);
       expect(_.isFunction(undefined)).equal(false);
@@ -131,8 +131,8 @@ describe('Lodash library', ()=>{
     });
   });
 
-  xdescribe('isNumber',  ()=>{
-    it('Should return true when passed any number', ()=>{
+  xdescribe('isNumber',  function(){
+    it('Should return true when passed any number', function(){
       expect(_.isNumber(12)).equal(true);
       expect(_.isNumber(1.2)).equal(true);
       expect(_.isNumber(12)).equal(true);
@@ -140,7 +140,7 @@ describe('Lodash library', ()=>{
       expect(_.isNumber(-10)).equal(true);
     });
 
-    it('Should return false otherwise', ()=>{
+    it('Should return false otherwise', function(){
       expect(_.isNumber({})).equal(false);
       expect(_.isNumber(NaN)).equal(false);
       expect(_.isNumber({})).equal(false);
@@ -148,8 +148,8 @@ describe('Lodash library', ()=>{
     });
   });
 
-  describe('forEach', ()=>{
-    it('should iterate over arrays, providing access to the element, index, and array itself', ()=> {
+  describe('forEach', function(){
+    it('should iterate over arrays, providing access to the element, index, and array itself', function() {
       const animals = ['ant', 'bat', 'cat'];
       const iterationInputs = [];
 
@@ -164,7 +164,7 @@ describe('Lodash library', ()=>{
       ]);
     });
 
-    it('should only iterate over the array elements, not properties of the array', ()=> {
+    it('should only iterate over the array elements, not properties of the array', function() {
       const animals = ['ant', 'bat', 'cat'];
       const iterationInputs = [];
 
@@ -181,7 +181,7 @@ describe('Lodash library', ()=>{
       ]);
     });
 
-    it('should iterate over objects, providing access to the element, index, and object itself', ()=> {
+    it('should iterate over objects, providing access to the element, index, and object itself', function() {
       const animals = { a: 'ant', b: 'bat', c: 'cat' };
       const iterationInputs = [];
 
@@ -197,46 +197,46 @@ describe('Lodash library', ()=>{
     });
   });
 
-  describe('map', ()=>{
-    it('should apply a function to every value in an array', ()=>{
+  describe('map', function(){
+    it('should apply a function to every value in an array', function(){
       const squaredValues = _.map([4, 2, 3, 9], (item) => item*item);
       expect(squaredValues).to.deep.equal([16, 4, 9, 81]);
     });
   });
 
-  xdescribe('slice', ()=>{
-    it('Should return a new array', ()=>{
+  xdescribe('slice', function(){
+    it('Should return a new array', function(){
       // TODO: write and implement the specs for this function
     });
   });
 
-  xdescribe('splice', ()=>{
-    it('Should return a new array', ()=>{
+  xdescribe('splice', function(){
+    it('Should return a new array', function(){
       // TODO: write and implement the specs for this function
     });
   });
 
-  describe('find: find does not mutate the array on which it is called.', ()=>{
+  describe('find: find does not mutate the array on which it is called.', function(){
 
-    xit('Should throw an error if no predicate is passed', ()=>{
+    xit('Should throw an error if no predicate is passed', function(){
         _.find().should.throw(Error);
     });
 
-    it('should return undefined if none of the elements match the predicate', ()=>{
+    it('should return undefined if none of the elements match the predicate', function(){
       const isEven = (num) => { num % 2 === 0; };
       const evens = _.find([1, 3, 7, 5], isEven);
       expect(evens).equal('undefined');
     });
 
-    it('should return the first element that matchs the predicate', ()=>{
+    it('should return the first element that matchs the predicate', function(){
       const isOdd = function(num){ num % 2 !== 0; };
       const odds = _.find([10, 2, 3, 4, 5, 6], isOdd);
       expect(odds).equal(3);
     });
   });
 
-  xdescribe('reduce', ()=> {
-    it('should be able to sum up an array', ()=> {
+  xdescribe('reduce', function() {
+    it('should be able to sum up an array', function() {
       const add = function(tally, item) {return tally + item; };
       const total = _.reduce([1, 2, 3], add, 0);
 
@@ -245,22 +245,22 @@ describe('Lodash library', ()=>{
 
   });
 
-  describe('contains', ()=> {
-    it('should return false if a collection does not contain a user-specified value', ()=> {
+  describe('contains', function() {
+    it('should return false if a collection does not contain a user-specified value', function() {
       expect(_.contains([4,5,6], 2)).to.equal(false);
     });
 
-    it('should return true if a collection contains a user-specified value', ()=> {
+    it('should return true if a collection contains a user-specified value', function() {
       expect(_.contains([  4,   5,   6], 5)).to.equal(true);
     });
 
-    it('can operate on objects', ()=>{
+    it('can operate on objects', function(){
       expect(_.contains({a:4, b:5, c:6}, 5)).to.equal(true);
     });
   });
 
-  describe('extend', ()=> {
-    it('returns the first argument', ()=> {
+  describe('extend', function() {
+    it('returns the first argument', function() {
       const to = {};
       const from = {};
       const extended = _.extend(to, from);
@@ -268,7 +268,7 @@ describe('Lodash library', ()=>{
       expect(extended).to.equal(to);
     });
 
-    it('should extend an object with the attributes of another', ()=> {
+    it('should extend an object with the attributes of another', function() {
       const to = {};
       const from = {a:'b'};
       const extended = _.extend(to, from);
@@ -276,7 +276,7 @@ describe('Lodash library', ()=>{
       expect(extended.a).to.equal('b');
     });
 
-    it('should override properties found on the destination', ()=> {
+    it('should override properties found on the destination', function() {
       const to = {a:'x'};
       const from = {a:'b'};
       const extended = _.extend(to, from);
@@ -284,7 +284,7 @@ describe('Lodash library', ()=>{
       expect(extended.a).to.equal('b');
     });
 
-    it('should not override properties not found in the source', ()=> {
+    it('should not override properties not found in the source', function() {
       const to = {x:'x'};
       const from = {a:'b'};
       const extended = _.extend(to, from);
@@ -292,19 +292,19 @@ describe('Lodash library', ()=>{
       expect(extended.x).to.equal('x');
     });
 
-    it('should extend from multiple source objects', ()=> {
+    it('should extend from multiple source objects', function() {
       const extended = _.extend({x:1}, {a:2}, {b:3});
 
       expect(extended).to.eql({x:1, a:2, b:3});
     });
 
-    it('in the case of a conflict, it should use the last property\'s values when extending from multiple source objects', ()=> {
+    it('in the case of a conflict, it should use the last property\'s values when extending from multiple source objects', function() {
       const extended = _.extend({x:'x'}, {a:'a', x:2}, {a:1});
 
       expect(extended).to.eql({x:2, a:1});
     });
 
-    it('should copy undefined values', ()=> {
+    it('should copy undefined values', function() {
       const extended = _.extend({}, {a: void 0, b: null});
 
       expect('a' in extended && 'b' in extended).to.be(true);
@@ -312,13 +312,13 @@ describe('Lodash library', ()=>{
   });
 
 
-  describe('findIndex', ()=>{});
+  describe('findIndex', function(){});
 
-  describe('fill', ()=>{});
+  describe('fill', function(){});
 
-  describe('keys', ()=>{});
+  describe('keys', function(){});
 
-  describe('values', ()=>{});
+  describe('values', function(){});
 
-  describe('entries', ()=>{});
+  describe('entries', function(){});
 });
