603883d64b346c36efe0b37e8830b205183433bb
Boubacar S. BARRY <bouba@hackages.io>
2016-11-03 14:29:24 +0100

    Cleanup of 06_context, instructions improved and new exercices added

--- test/06_context.spec.js
+++ test/06_context.spec.js
@@ -1,39 +1,165 @@
+const expect = require('chai').expect;
+require('chai').should();
+
 /**
 * Context in JavaScript
 **/
-const expect = require('chai').expect;
 
-xdescribe('Context in JavaScript', () => {
+describe('Context in JavaScript', () => {
 
-  var company = {
-    name: 'Hackages Lab',
-    mentors: ['Alex', 'Brecht', 'Davy'],
-    addMentor: function(name){
-      this.mentors.push('Juan');
-      return this;
+
+
+
+  describe('Should use call & apply', () => {
+
+    function upperCase() {
+      return this.toUpperCase();
     }
-  };
 
-  it('What is "this" refer to?', () => {
-    var context = company.addMentor();
-    // Fill in the expectation
-    expect().to.equal();
+    function toFixed(numberOfDecimals) {
+      return this.toFixed(numberOfDecimals);
+    }
 
-    //-------------------------------------------------------
-    var context = function(){
-      return function init(){
-        return this;
-      }();
-    };
+    function concat(secondArray) {
+      return this.concat(secondArray);
+    }
 
-    var result = context();
-    // Fill in the expectation
-    expect().to.equal();
+    it('with call', () => {
+
+      // TODO Modify the code above to satisfy all the assertions below.
+      const actual = upperCase();
+
+      expect(actual).equal('JUAN');
+    });
+
+    it('with call and arguments', () => {
+
+      const numberToUse = 5.56789;
+
+      // TODO Modify the code above to satisfy all the assertions below.
+      const actual = toFixed();
+
+      expect(actual).equal('5.57');
+    });
+
+    it('with apply and arguments', () => {
+
+      // TODO Modify the code above to satisfy all the assertions below.
+      const actual = concat();
+
+      expect(actual).eql([1, 2, 3, 4]);
+    });
   });
 
-  it('Should switch the context', () => {
-    var context = company.addMentor();
-    expect(context).to.equal({name: 'Hackages'});
+  describe('Should use bind', () => {
+
+    it('increment the counter value', () => {
+
+      const counter = {
+        value: 0,
+      };
+      // TODO Modify the code above to satisfy all the assertions below.
+      function increment() {
+        this.value++;
+        if (this.value === 10) {
+          expect(this.value).equal(10);
+          done();
+        }
+      }
+      setInterval(increment, 1000);
+    });
+
+    it('push a new user after 2s', (done) => {
+
+      const hackcamp = {
+        members: ['Davy']
+      };
+      // TODO Modify the code above to satisfy all the assertions below.
+      function addMembers() {
+        this.members.push('Joker');
+        expect(this.members.length).equal(2);
+        done();
+      }
+      setTimeout(addMembers, 2000);
+    });
+  });
+
+  describe('What does "this" refer to ?', () => {
+
+    function addMentor(mentor) {
+      this.mentors.push(mentor);
+      return this.mentors;
+    }
+
+    it('Should add new mentor ', () => {
+
+      const company = {
+        name: 'Hackages Lab',
+        addMentor: addMentor,
+      };
+      const actual = company.addMentor('Juan');
+
+      expect(actual).to.be.equal(['Alex', 'Juan']);
+    });
+
+    it('Should switch the context', () => {
+
+      const hackcamp = {
+        mentors: ['Eric', 'Martine']
+      };
+      const davy = 'Joker';
+
+      // TODO Modify the code above to satisfy all the assertions below.
+      let actual = addMentor(davy);
+
+      expect(actual).to.equal(['Eric', 'Martine', 'Joker']);
+
+      const hackjam = {
+        mentors: ['Harold', 'Bouba', 'Ilyes'],
+      };
+
+      // TODO Modify the code above to satisfy all the assertions below.
+      actual = addMentor(davy);
+
+      expect(actual).to.equal(['Harold', 'Bouba', 'Ilyes', 'Joker']);
+    });
+
+  });
+
+  describe('More about context', () => {
+
+    it("with 'new' keywords ", () => {
+
+      // TODO Modify the code above to satisfy all the assertions below.
+      function User(args) {
+        function init() {
+          this.name = args.name;
+        }
+      }
+      const actual = new User({ name: 'Hackages' });
+
+      expect(actual).eql({ name: 'Hackages' });
+    });
+
+    it("without 'new' keywords ", () => {
+      /**
+       * TODO
+       * Start from the solution you found above.
+       * Modify the function to solve the assertion
+       * Also, this time try not to use the 'new' keyword
+       * and simply call the User function instead.
+       * Ex: const user = User({ name: 'Hackages' })
+       */
+      function User(args) {
+        function init() {
+          this.name = args.name;
+        }
+      }
+
+      const actual = {}; // TODO
+
+      expect(actual).eql({ name: 'Hackages' });
+    });
   });
 
 });
